ad35d7f2e4f26681f631f517f454c445540fc43e
0xwhyteddy <0xwhyteddy@gmail.com>
2024-12-27 09:13:17 +0700

    Update install-script.sh

--- install-script.sh
+++ install-script.sh
@@ -12,20 +12,20 @@
  apt install python3-pip -y
  apt-get install -y python3
 
-# Install Python packages    
-  apt-get install -y git python3-venv
-  python -m venv venv
-  source venv/bin/activate
-  pip install --upgrade pip
-  pip install cloudscraper
-  pip install requests
-  pip install asyncio
-  pip install aiohttp
-  pip install loguru
-  pip install cloudscraper
-  pip install curl_cffi
-  pip install fake_useragent
-  
+#Install Python 3.10
+apt update && sudo apt upgrade -y
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa
+apt install python3.10
+apt install python3-pip
+#Install Requirements
+pip install requests
+pip install asyncio
+pip install aiohttp
+pip install loguru
+pip install cloudscraper
+pip install curl_cffi
+pip install fake_useragent==1.5.1
 # Clone repository
 git clone https://github.com/0xwhyteddy/nodepay.git
 
