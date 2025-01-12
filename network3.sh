0e2882d31077b9abbb41479cd92c09d7c38f90c6
0xwhyteddy <0xwhyteddy@gmail.com>
2025-01-13 02:12:37 +0700

    Update network3.sh

--- network3.sh
+++ network3.sh
@@ -10,15 +10,16 @@ print_green() {
 
 # Update dan upgrade sistem
 echo "Melakukan update dan upgrade sistem..."
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Install net-tools
 echo "Menginstal net-tools..."
-sudo apt install net-tools -y
+apt install net-tools -y
 
-sudo ufw allow 8080
-sudo ufw enable
-sudo ufw status
+# ufw tidak perlu dijalankan di GitLab CI
+# sudo ufw allow 8080
+# sudo ufw enable
+# sudo ufw status
 
 # Download dan ekstrak file
 echo "Menyiapkan direktori dan mendownload file..."
@@ -30,15 +31,15 @@ sleep 2
 # Masuk ke folder ubuntu-node dan menjalankan manager.sh
 echo "Menjalankan manager.sh..."
 cd ubuntu-node
-sudo bash manager.sh up
+bash manager.sh up
 
 # Dapatkan private key dan hubungkan
 echo "Mengambil private key"
-sudo bash manager.sh key | print_green
+bash manager.sh key | print_green
 
 # Menentukan IP VPS
-VPS_IP=$(hostname -I | awk '{print $1}')
+# VPS_IP=$(hostname -I | awk '{print $1}')  # Tidak diperlukan di GitLab CI
 
 # Menampilkan URL dan instruksi untuk mengganti IP dengan IP VPS setelah proses selesai
-echo "Done. Silakan buka web dengan link dibawah dan koneksikan private keymu"
-print_green "https://account.network3.ai/main?o=${VPS_IP}:8080"
+echo "Done." 
+# print_green "https://account.network3.ai/main?o=${VPS_IP}:8080" # Tidak diperlukan di GitLab CI
