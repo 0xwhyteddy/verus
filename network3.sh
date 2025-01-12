5cd94644db402fa24cbc928720a825e5ba456d21
0xwhyteddy <0xwhyteddy@gmail.com>
2025-01-13 02:17:03 +0700

    Update network3.sh

--- network3.sh
+++ network3.sh
@@ -3,23 +3,18 @@
 curl -s https://data.zamzasalim.xyz/file/uploads/asclogo.sh | bash
 sleep 2
 
-# Fungsi untuk menampilkan teks berwarna hijau
-print_green() {
-    echo -e "\e[32m$1\e[0m"
-}
 
 # Update dan upgrade sistem
 echo "Melakukan update dan upgrade sistem..."
-apt update && apt upgrade -y
+apt update && sudo apt upgrade -y
 
 # Install net-tools
 echo "Menginstal net-tools..."
 apt install net-tools -y
 
-# ufw tidak perlu dijalankan di GitLab CI
-# sudo ufw allow 8080
-# sudo ufw enable
-# sudo ufw status
+ufw allow 8080
+ufw enable
+ufw status
 
 # Download dan ekstrak file
 echo "Menyiapkan direktori dan mendownload file..."
@@ -35,11 +30,11 @@ bash manager.sh up
 
 # Dapatkan private key dan hubungkan
 echo "Mengambil private key"
-bash manager.sh key | print_green
+bash manager.sh key |
 
 # Menentukan IP VPS
-# VPS_IP=$(hostname -I | awk '{print $1}')  # Tidak diperlukan di GitLab CI
+VPS_IP=$(hostname -I | awk '{print $1}')
 
 # Menampilkan URL dan instruksi untuk mengganti IP dengan IP VPS setelah proses selesai
-echo "Done." 
-# print_green "https://account.network3.ai/main?o=${VPS_IP}:8080" # Tidak diperlukan di GitLab CI
+echo "Done. Silakan buka web dengan link dibawah dan koneksikan private keymu"
+print_green "https://account.network3.ai/main?o=${VPS_IP}:8080"
